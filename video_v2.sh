30ef08f58c1ad559a5098433760c5cc9a65e173d
tamnln2 <tamnln2@fpt.com>
2024-09-30 15:24:32 +0700

    creare

---
+++ video_v2.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector demo data_firev6/yolo.data MobileNetV2-YOLO-Fastest/v2/MobileNetV2-YOLO-Fastest-voc-v2.cfg data_firev6/backup/MobileNetV2-YOLO-Fastest-voc-v2_last.weights data_firev6/videoplayback.mp4 -i 0 -thresh 0.25
+
+
+
